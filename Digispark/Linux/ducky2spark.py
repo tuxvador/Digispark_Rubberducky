a8d93ac115748d349fba2f680babb9b9e7217066
Unknown <tuxvador411@gmail.com>
2017-09-08 09:40:50 +0200

    Linux version 1.0.1

--- Digispark/Linux/ducky2spark.py
+++ Digispark/Linux/ducky2spark.py
@@ -8,33 +8,51 @@
 
 #imports
 import os
+from fnmatch import fnmatch
 from subprocess import Popen,PIPE
 
 #Strings used in programme
-cf2c = "Choose .duck files to convert"
-cb2i = "Concert binary file to arduino script"
-cd2i = "Convert ducky script to arduino"
+cf2c = "Choose file to convert : "
+cb2i = "Convert ducky script to arduino script"
+cd2i = "Concert binary file to arduino script"
 wiyc = "What is your choice : "
+convbin = "Converting file to bin"
+convino = "Converting file to ino"
+duckencoder = "java -jar ./exes/duckencoder.jar -i"
 
-def choosfile():
-    directory = Popen("ls ./scripts", stdout=PIPE)
-    output = "..\n" + directory.stdout.read()
-    output = output.split('\n')
-    output.pop(-1)
+def choosfile(choix):
     files = ""
-    print output
+    path = ""
+    mapping = ""
+    length = 0
+
+    if choix == 1:
+        path = "./bin/"
+    if choix == 2:
+        path = "./scripts/"
+
+    for files in os.walk(path):
+        length = len(files)
+        files = files[2]
+
+    choix = -1
+    for i,j in enumerate(files):
+        print(str(i+1)+") "+j)
+    while choix not in range(0,length):
+        choix = int(raw_input(cf2c))-1
+    path = path+files[choix]
+    ifbin = path.find("./bin/")
+    if(ifbin < 0):
+        execute = os.popen(duckencoder).read()
+        print(execute)
 
 def main():
     choix = ""
-    while(choix not in ['1','2']):
+    while(choix not in [1,2]):
         print("1) "+cd2i)
         print("2) "+cb2i)
-        choix = raw_input(wiyc)
-    if choix == '1':
-        print("[+] You chose to convert from binary to ino script")
-    else:
-        print("[+] You chose to convert from ducky script to ino script")
-    choosfile()
+        choix = int(raw_input(wiyc))
+    choosfile(choix)
 
 
 if __name__ == '__main__':
